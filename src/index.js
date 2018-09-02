f3e4a00b89131f1d2a071c84d055cc46630cb0d6
victorjaquez <victorajaquez@gmail.com>
2018-09-02 01:05:51 -0700

    fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Store from "./store";
-import "./index.css";
+import "./App.css";
 
 const store = Store();
 
